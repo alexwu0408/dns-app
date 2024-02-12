45e55475f8838234fb116044799947cb97bff2c1
adong850408 <adong850408@gmail.com>
2024-02-11 21:56:56 -0500

    docker created and define the route /time

--- sample_time_app/run.py
+++ sample_time_app/run.py
@@ -1,12 +1,23 @@
 from flask import Flask
+from datetime import datetime, timedelta
+
 app = Flask(__name__)
 
-
+# Define the domain route
 @app.route('/')
 def hello_world():
     return 'Hello world!'
+
+# Get the current time
+@app.route('/time')
+def get_current_time():
+    current_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    return f"The current time is: {current_time}" 
 
 
 app.run(host='0.0.0.0',
         port=8080,
         debug=True)
+
+
+
